3539396d27440941eee0de79781acf4d660c348f
Vincent Ambo <tazjin@gmail.com>
2016-05-07 15:17:38 +0200

    [web] Add initial Favicon, but needs a better one

--- lib/goon_auth/endpoint.ex
+++ lib/goon_auth/endpoint.ex
@@ -9,7 +9,7 @@ defmodule GoonAuth.Endpoint do
   # when deploying your static files in production.
   plug Plug.Static,
     at: "/", from: :goon_auth, gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    only: ~w(css fonts images js favicon.png robots.txt)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
