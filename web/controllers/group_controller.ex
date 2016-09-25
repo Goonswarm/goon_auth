37dab05a65fac74eb8198252f72935dcdb961f39
Vincent Ambo <tazjin@gmail.com>
2016-09-25 20:20:49 +0200

    [web/groups] Check permissions for editing groups
    * Checks that the user accessing a certain group has permission to do so
    * Fixes rendering of groups with only one member / owner

--- web/controllers/group_controller.ex
+++ web/controllers/group_controller.ex
@@ -2,25 +2,57 @@ defmodule GoonAuth.GroupController do
   @moduledoc """
   Simple pages to support basic LDAP group management.
   """
-  use GoonAuth.Web, :controller
   alias GoonAuth.LDAP
   import GoonAuth.Auth, only: [authenticate: 2]
+  require Logger
+  use GoonAuth.Web, :controller
 
   # All requests to this controller must be authenticated
   plug :authenticate
 
+  @doc """
+  Fetches all groups that a user belongs to, as well as all groups that a user
+  has permission to manage and displays them.
+  """
   def group_list(conn, _params) do
     user                  = get_session(conn, :user)
     {:ok, ldap_conn}      = LDAP.connect
     {:ok, groups}         = LDAP.find_groups(ldap_conn, user, :group)
     {:ok, managed_groups} = LDAP.find_groups(ldap_conn, user, :group, 'owner')
-    render(conn, "groups.html", groups: groups, managed_groups: managed_groups)
+
+    render(conn, "groups.html",
+      groups: groups,
+      managed_groups: managed_groups)
   end
 
+  @doc """
+  Shows detailed information about a group and provides the ability to
+  add/remove group members.
+  Only group managers can open this page.
+  """
   def group_view(conn, %{"name" => name}) do
     user = get_session(conn, :user)
     {:ok, ldap_conn} = LDAP.connect
     {:ok, group} = LDAP.retrieve(ldap_conn, name, :group)
-    render(conn, "group.html", group: group)
+
+    if has_access?(group["owner"], user) do
+      render(conn, "group.html", group: group)
+    else
+      Logger.warn("User #{user} attempted to view group #{name}!")
+      conn
+      |> put_flash(:error, "You don't have permission to manage this group!")
+      |> redirect(to: "/groups")
+    end
+  end
+
+  defp has_access?(group_owners, user) when is_list(group_owners) do
+    dn_fragment = "cn=#{user},"
+    Enum.any?(group_owners, fn(owner) ->
+      String.contains?(owner, dn_fragment)
+    end)
+  end
+  defp has_access?(owner, user) when is_binary(owner) do
+    dn_fragment = "cn=#{user},"
+    String.contains?(owner, dn_fragment)
   end
 end
--- web/views/group_view.ex
+++ web/views/group_view.ex
@@ -1,3 +1,12 @@
 defmodule GoonAuth.GroupView do
   use GoonAuth.Web, :view
+
+  # If only a single owner / member is present on a group, the type of that
+  # field changes - however we always expect a list. This normalises it.
+  def listify_users(user_list) when is_list(user_list) do
+    user_list
+  end
+  def listify_users(user) when is_binary(user) do
+    [user]
+  end
 end
